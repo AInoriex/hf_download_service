81aa519d2d3ffc3bcce87d301f0c59c5f2768dfb
AInoriex <alexinori@163.com>
2025-03-04 17:15:10 +0800

    [feat] 补充注释，增加下载间隔；

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ load_dotenv()
 
 import os
 from huggingface_hub import hf_hub_download, HfApi
-from utils import obs, ufile
+from utils import obs, ufile, utime
 from utils.logger import logger
 
 # HF仓库信息
@@ -14,8 +14,17 @@ hf_token = os.getenv("HF_TOKEN")
 assert hf_token
 logger.info(f"[Huggingface Info] repo_id:{repo_id}, repo_type:{repo_type}, revision:{revision}, hf_token:{hf_token}")
 
-# 列举hf文件
 def hf_list_files(repo_id, revision="main"):
+    """
+    通过Huggingface Hub API获取HF仓库下的所有文件
+
+    Args:
+        repo_id (str): 仓库ID
+        revision (str, optional): 仓库分支. Defaults to "main".
+
+    Returns:
+        list: 仓库下所有文件
+    """
     api = HfApi()
     # repo_id = "MLCommons/unsupervised_peoples_speech"
     files = api.list_repo_files(repo_id=repo_id, revision=revision, repo_type="dataset", token=hf_token)
@@ -43,6 +52,14 @@ def hf_batch_download_concurrent(file_list:list, repo_id:str, download_file_path
             print(f"第 {futures.index(future) + 1} 批文件下载完成")
 
 def _hf_download_handler(file_list, repo_id, repo_type, revision, download_file_path):
+    """
+    hf_batch_download_concurrent的handler
+    :param file_list: 文件名列表
+    :param repo_id: 仓库ID
+    :param repo_type: 仓库类型
+    :param revision: 仓库分支
+    :param download_file_path: 文件下载路径
+    """
     for file in file_list:
         try:
             print(f"当前正在下载: {file} \n")
@@ -57,12 +74,25 @@ def _hf_download_handler(file_list, repo_id, repo_type, revision, download_file_
                 force_download=False,
             )
         except Exception as e:
-            print(f"下载失败: {file}, 错误: {e}")
+            print(f"[FAILED] {file} 下载失败, 错误: {e}")
             logger.error(f"{repo_id}.{revision} -> {download_file_path}/{file}下载失败, 错误: {e}")
             # ufile.write_string_to_file(text_string="", out_file=f"{download_index_path}/{file}.fail")
             continue
+        else:
+            print(f"[SUCCESS] {file} 下载成功 \n")
+        finally:
+            utime.random_sleep(1, 2)
 
 def read_files_list_from_file(index_file:str)->list:
+    """
+    Reads a list of file names from a specified index file.
+
+    Args:
+        index_file (str): The path to the index file containing file names.
+
+    Returns:
+        list: A list of file names read from the index file, with each file name stripped of leading and trailing whitespace.
+    """
     ret_list = []
     with open(index_file, "r", encoding="utf8") as f:
         lines = f.readlines()
